8a0da39a0e6cbb8899c73bc1bea6b52e1bd2b9a4
wouter van dijk <wouterrobot@hotmail.nl>
2017-07-10 10:03:27 +0200

    Will open a trailing stop order if price hits a certain threshold

--- Kraken Automated Trading/Kraken_Automated_Trading.py
+++ Kraken Automated Trading/Kraken_Automated_Trading.py
@@ -1,11 +1,46 @@
 import krakenex
+from time import sleep
 import Keys
 import pprint
 import json
 
-
+sellTrigger = 235
+buyPrice = 0
+currentOrder = 1
+DidTransactionHappen = False
+SellOrderAlreadyCreated = False
 k = krakenex.API(Keys.APIKey,Keys.privateKey)
-response = k.query_public('Depth', {'pair': 'XETHZEUR', 'count': '1'})
-pprint.pprint(json.dumps(response))
-x = json.dumps(response['result'])
-print(response['result']['XETHZEUR']['bids'][0][0])
+currentPrice = 0
+
+def TrailingSellOrder():
+    k.query_private('AddOrder', {'pair': 'XETHZEUR',
+                                 'type': 'sell',
+                                 'ordertype': 'trailing-stop',
+                                 'price': '#5%',
+                                 'volume': '1.5',
+                                 'userref': str(currentOrder)})
+    print("sell order created")
+    sleep(4)
+
+
+while not DidTransactionHappen:
+    try:
+        response = k.query_public('Depth', {'pair': 'XETHZEUR', 'count': '1'})
+        x = json.dumps(response['result'])
+        currentPrice = response['result']['XETHZEUR']['asks'][0][0]
+        print(currentPrice)
+        #z = k.query_private('OpenOrders',{'userref': str(currentOrder)})
+        #pprint.pprint(z['result'])
+        #sleep(1)
+        if(float(currentPrice) > sellTrigger and SellOrderAlreadyCreated == False):
+            print('If statement is triggered')
+            TrailingSellOrder()
+            SellOrderAlreadyCreated = True
+            DidTransactionHappen = True
+    except Exception as e: print(e)
+
+    sleep(30)
+
+
+
+
